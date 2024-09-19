28aeb1a669b0c9c15dbd1ac07d97a9dcda3ec616
Igor Wanderley <igorfwds@hotmail.com>
2024-09-19 18:00:57 -0300

    Initial Commit

---
+++ ChefDelivery/ChefDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryApp.swift
+//  ChefDelivery
+//
+//  Created by ifws on 19/09/24.
+//
+
+import SwiftUI
+
+@main
+struct ChefDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
