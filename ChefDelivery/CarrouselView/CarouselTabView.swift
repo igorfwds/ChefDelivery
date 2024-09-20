3fefd2393ea443acf0c616958d5ee8f3d28dd16a
Igor Wanderley <igorfwds@hotmail.com>
2024-09-20 17:12:06 -0300

    First part of Carousel componnent done

---
+++ ChefDelivery/CarrouselView/CarouselTabView.swift
@@ -0,0 +1,29 @@
+//
+//  CarouselTabView.swift
+//  ChefDelivery
+//
+//  Created by ifws on 20/09/24.
+//
+
+import SwiftUI
+
+struct CarouselTabView: View {
+    
+    let ordersMock: [OrderType] = [
+        OrderType(id: 1, name: "banner burguer", image: "barbecue-banner"),
+        OrderType(id: 2, name: "banner prato feito", image: "brazilian-meal-banner"),
+        OrderType(id: 3, name: "banner poke", image: "pokes-banner"),
+    ]
+    
+    var body: some View {
+        TabView {
+            ForEach (ordersMock) { banner in
+                CarrouselItemView(order: banner)
+            }
+        }
+    }
+}
+
+#Preview {
+    CarouselTabView()
+}
