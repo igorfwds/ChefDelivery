f395323d17ad0c1b1426cf652f79dc9b4545294d
Igor Wanderley <igorfwds@hotmail.com>
2024-09-20 14:44:58 -0300

    OrderTypeView component integrated

---
+++ ChefDelivery/Model/DataSourceMock.swift
@@ -0,0 +1,18 @@
+//
+//  DataSourceMock.swift
+//  ChefDelivery
+//
+//  Created by ifws on 20/09/24.
+//
+
+import Foundation
+
+let ordersMock: [OrderType] = [
+    OrderType(id: 1 , name: "Restaurantes", image: "hamburguer"),
+    OrderType(id: 2 , name: "Mercado", image: "mercado"),
+    OrderType(id: 3 , name: "Farmácia", image: "farmacia"),
+    OrderType(id: 4 , name: "Pet", image: "petshop"),
+    OrderType(id: 5 , name: "Descontos", image: "descontos"),
+    OrderType(id: 6 , name: "Bebidas", image: "bebidas"),
+    OrderType(id: 7 , name: "Gourmet", image: "gourmet"),
+]
